6c2a2ab3de4905b6a9605d5edac1f274f50d234d
Michael Allan <michaelallan@optushome.com.au>
2013-11-10 18:06:12 +1100

    Expose UserService to MainCtrl

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-.controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope, UserService) {
     $scope.signedIn = function (oauth) {
         UserService.setCurrentUser(oauth)
         .then(function (user) {
