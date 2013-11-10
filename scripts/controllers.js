f92a73237a9a56b3bfc22b0285fc2b386c7aa0c7
Michael Allan <michaelallan@optushome.com.au>
2013-11-10 18:00:15 +1100

    Add UserService called by signedIn action

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,4 +1,9 @@
 angular.module('myApp')
 .controller('MainCtrl', function ($scope) {
-    
+    $scope.signedIn = function (oauth) {
+        UserService.setCurrentUser(oauth)
+        .then(function (user) {
+            $scope.user = user;
+        });
+    }
 });
--- scripts/services.js
+++ scripts/services.js
@@ -1,1 +1,25 @@
-angular.module('myApp.services', []);
+angular.module('myApp.services', [])
+.factory('UserService', function ($q, $http) {
+    var service = {
+        _user: null,
+
+        setCurrentUser: function (u) {
+            if (u && !u.error) {
+                service._user = u;
+                return service.currentUser();
+            } else {
+                var d = $q.defer();
+                d.reject(u.error);
+                return d.promise;
+            }
+        },
+
+        currentUser: function () {
+            var d = $q.defer();
+            d.resolve(service._user);
+            return d.promise;
+        }
+    };
+
+    return service;
+});
