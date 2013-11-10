ce29db174145ac0153c4f9e13962473577227ad7
Michael Allan <michaelallan@optushome.com.au>
2013-11-10 15:50:11 +1100

    Add stripe, g+ and app bootstrap sequence

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 'use strict';
 
+// callback from G+
+window.appInit = function () {
+    // when the document is ready
+    angular.element(document).ready(function () {
+        // bootstrap the oauth2 library
+        gapi.client.load('oauth', 'v2', function () {
+            // and finally, bootstrap our angular app
+            angular.bootstrap(document, ['myApp']);
+        });
+    });
+};
+
+
 angular.module('myApp', [
     'ngRoute',
     'myApp.services',
